79bdae8969db487c52ed2e37ff6c3af01f10db5d
Matthew Griffin <matthew.griffin@geoteric.com>
2020-12-02 07:41:31 +0000

    Initial commit

---
+++ advent-code-2020/Day1Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace advent_code_2020
+{
+    public class Day1Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
